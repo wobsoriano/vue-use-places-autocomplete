9045f3fba3f94fcfb96dd361e61691afb1a73111
Robert Soriano <sorianorobertc@gmail.com>
2021-08-04 20:40:07 +0800

    update demo

--- packages/demo/src/App.vue
+++ packages/demo/src/App.vue
@@ -7,12 +7,23 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import usePlacesAutocomplete from 'v-use-places-autocomplete'
+import { ref, watchEffect } from 'vue'
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'v-use-places-autocomplete'
 
 const query = ref('manila')
 const { suggestions, loading } = usePlacesAutocomplete(query, {
   apiKey: import.meta.env.VITE_PLACES_API_KEY as string,
   minLengthAutocomplete: 2
+})
+
+watchEffect(async () => {
+  if (suggestions.value.length) {
+    const geocode = await getGeocode({
+      placeId: suggestions.value[0].place_id
+    });
+    console.log('getGeocode result: ', geocode)
+    const latLng = await getLatLng(geocode[0]);
+    console.log('getLatLng result: ', latLng);
+  }
 })
 </script>
