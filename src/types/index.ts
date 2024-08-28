ca2342828d51cc4e25458ce334ec7acfc11bd89c
wobsoriano <sorianorobertc@gmail.com>
2024-08-28 13:27:59 -0700

    chore: Add missing autocompletion request types

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,10 +5,39 @@ export interface LatLng {
   lng: number
 }
 
+export interface LatLngLiteral {
+  latDegrees: number
+  lngDegrees: number
+}
+
+export interface LatLngBoundsLiteral {
+  east: number
+  north: number
+  south: number
+  west: number
+}
+
+export interface LatLngBounds {
+  sw: LatLng | LatLngLiteral | LatLngBounds | LatLngBoundsLiteral
+  ne: LatLng | LatLngLiteral
+}
+
 export interface AutocompletionRequest {
   bounds?: [LatLng, LatLng]
   componentRestrictions?: { country: string | string[] }
+  /**
+   * Location for prediction biasing. Predictions will be biased towards the
+   * given <code>location</code> and <code>radius</code>. Alternatively,
+   * <code>bounds</code> can be used.
+   * @deprecated <code>location</code> is deprecated as of May 2023.
+   *     Use {@link google.maps.places.AutocompletionRequest.locationBias}
+   *     and {@link
+   *     google.maps.places.AutocompletionRequest.locationRestriction}
+   *     instead.
+   */
   location?: LatLng
+  locationBias?: LatLng | LatLngLiteral | LatLngBoundsLiteral | string
+  locationRestriction?: LatLngBounds | LatLngBoundsLiteral
   offset?: number
   radius?: number
   types?: string[]
